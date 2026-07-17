97cc4ec836105afd9eb8af164852a3b47b42863f
Nirmalya <gangulynirmalya99@gmail.com>
2026-07-18 00:00:43 +0530

    Document middleware and validations

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
+      minlength: [6, 'A tour name must have atleast 6 characters'],
+      maxlength: [40, 'A tour name must have atmost 40 characters'],
       unique: true,
     },
     duration: {
@@ -18,10 +21,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -34,11 +43,21 @@ const tourSchema = new mongoose.Schema(
     priceDiscount: {
       type: Number,
       default: 0,
+      // Custom validator to check if discount is less than price
+      validate: {
+        validator: function (value) {
+          return this.price > value;
+        },
+        message: 'Discount price should be below regular price',
+      },
     },
     summary: {
       type: String,
       required: [true, 'A tour must have a summary'],
       trim: true,
+    },
+    slug: {
+      type: String,
     },
     description: {
       type: String,
@@ -70,6 +89,18 @@ const tourSchema = new mongoose.Schema(
 // the value of other attribute
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// Document middleware
+// Pre-save middleware -> This middleware function gets executed right before the document is getting saved into the DB
+tourSchema.pre('save', function () {
+  // Here this will point to the document that is getting saved to database
+  this.slug = slugify(this.name, { lower: true });
+});
+
+// Post-save middleware -> This middleware function gets executed right after the document is saved to the DB
+tourSchema.post('save', function (doc) {
+  console.log(doc);
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
