eb6c7aa5de8abb4fe760d78f7abb6aa2597ba121
Nirmalya <gangulynirmalya99@gmail.com>
2026-07-18 18:24:56 +0530

    Implement error handling

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+
+// Uncaught exception
+process.on('uncaughtException', (error) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down..');
+  console.log(error.name, error.message);
+  process.exit(1);
+});
 
 const app = express();
 
@@ -31,5 +40,14 @@ app.use((req, res, next) => {
 // Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handle invalid routes
+app.all('*splat', (req, res, next) => {
+  const appError = new AppError(`Cannot access ${req.originalUrl} on the server`, 404);
+  next(appError);
+});
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 module.exports = app;
