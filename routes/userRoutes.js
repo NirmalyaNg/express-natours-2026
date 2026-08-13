75e9bdadab05bd6d12ba6c37c7ec8c491f8ec6f9
Nirmalya <gangulynirmalya99@gmail.com>
2026-08-13 11:07:32 +0530

    Revert main to day-13 original content
    Removes auth/security/user-management features added via the
    day-10-self..day-13-self branch chain, restoring main to the
    day-12 + 'Handling Operational Errors' state.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,17 @@
 const { Router } = require('express');
 const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
-const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = Router();
 
-router.route('/signup').post(authController.signup);
-router.route('/login').post(authController.login);
-router.route('/forgotPassword').post(authController.forgotPassword);
-router.route('/resetPassword/:resetToken').patch(authController.resetPassword);
-router.route('/changeMyPassword').patch(authMiddleware.protect, authController.changeMyPassword);
 router
-  .route('/me')
-  .patch(authMiddleware.protect, userController.updateMe)
-  .delete(authMiddleware.protect, userController.deleteMe);
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
-router.route('/:id').get(userController.getUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
