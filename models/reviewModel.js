fddb226aac6c9c75dce6d1cdb7725253a864e61a
Nirmalya <gangulynirmalya99@gmail.com>
2026-09-01 23:44:08 +0530

    update review stats in tour for review operations

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('../models/tourModel');
 
 const reviewSchema = new mongoose.Schema({
   review: {
@@ -24,6 +25,53 @@ const reviewSchema = new mongoose.Schema({
     ref: 'User',
     required: [true, 'Review must belong to an user'],
   },
+});
+
+reviewSchema.statics.updateTourReviewStats = async function (tourId) {
+  const reviewStats = await this.aggregate([
+    {
+      $match: {
+        tour: tourId,
+      },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        numRatings: {
+          $sum: 1,
+        },
+        averageRating: {
+          $avg: '$rating',
+        },
+      },
+    },
+  ]);
+  if (reviewStats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: reviewStats[0].averageRating,
+      ratingsQuantity: reviewStats[0].numRatings,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+    });
+  }
+};
+
+// Update the ratingsAverage and ratingsQuantity on a tour when its review is created/updated/deleted
+reviewSchema.post('save', function (doc) {
+  const tourId = doc.tour;
+  doc.model().updateTourReviewStats(tourId);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function () {
+  const r = await this.model.findOne(this.getQuery());
+  this.r = r;
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  this.model.updateTourReviewStats(this.r.tour._id);
 });
 
 // Populate user and tour for review
